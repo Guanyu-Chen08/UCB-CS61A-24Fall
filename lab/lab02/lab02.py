88b68c3a0308af9afec4a5a9e8aae70eac61192a
CGY <chenguanyu@mail.ustc.edu.cn>
2025-06-12 22:43:15 +0800

    Add files via upload

--- lab/lab02/lab02.py
+++ lab/lab02/lab02.py
@@ -125,7 +125,7 @@ def cycle(f1, f2, f3):
                 if times == 2:
                     return f2(f1(x))
                 x = f3(f2(f1(x)))
-                times -= 3     
+                times = times - 3
         return h
     return g
         
